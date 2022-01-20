20ab023b160b9c1287eed91c9081527adf4d80eb
mikan1236 <fumi4748.ait@gmail.com>
2022-01-20 10:35:01 +0900

    Default Changelist

--- ysp_hp/settings.py
+++ ysp_hp/settings.py
@@ -164,7 +164,7 @@ LOGGING = {
 
 
     'handlers': {
-        'console': {
+        'file': {
             'level': 'INFO',
             'class': 'logging.handlers.TimedRotatingFileHandler',
             'filename': os.path.join(BASE_DIR, 'logs/django.log'),
